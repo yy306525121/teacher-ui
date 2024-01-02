9b64880fde3609912d4f069ba80dbd1662a28ae1
yangzy <306525121@qq.com>
2024-01-02 17:00:54 +0800

    fullcalendar测试页面

---
+++ src/views/course/test/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="app-container">
+    <FullCalendar :options='calendarOptions'>
+      <template v-slot:eventContent='arg'>
+        <b>{{ arg.event.title }}</b>
+      </template>
+    </FullCalendar>
+  </div>
+</template>
+
+<script setup name="Test">
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import { reactive } from "vue";
+
+const calendarOptions = reactive({
+  plugins: [ dayGridPlugin, interactionPlugin, timeGridPlugin],
+  locale: 'zh',
+  initialView: 'timeGridWeek',
+})
+</script>
+
+<style scoped>
+
+</style>
